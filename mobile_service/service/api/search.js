2fc3600f5d46c96eaf83fb0653aa3b7e26878534
Giorgi Kandelaki <kandelakig@gmail.com>
2013-11-16 23:17:08 +0400

    changes in search api

--- mobile_service/service/api/search.js
+++ mobile_service/service/api/search.js
@@ -1,17 +1,21 @@
 exports.get = function(request, response) {
 	var mssql = request.service.mssql
-	var text = request.query.q + '%'
+	var text = (request.query.q || '') + '%'
 
 	function handleError(err) {
+		console.error(statusCodes.INTERNAL_SERVER_ERROR)
 		console.error(err)
 		response.send(statusCodes.INTERNAL_SERVER_ERROR, err)
 	}
 
 	var result = {}
+    
+    var oneComplete = false
 
 	mssql.query('SELECT * FROM merchants m WHERE m.name LIKE ?', [text], {
 		success: function(res) {
 			result.merchants = res
+            oneComplete = oneComplete ? response.send(statusCodes.OK, result) : true
 		},
 		error: handleError
 	})
@@ -19,9 +23,8 @@ exports.get = function(request, response) {
 	mssql.query('SELECT * FROM items m WHERE m.name LIKE ?', [text], {
 		success: function(res) {
 			result.items = res
+            oneComplete = oneComplete ? response.send(statusCodes.OK, result) : true
 		},
 		error: handleError
 	})
-
-    response.send(statusCodes.OK, result);
 };
